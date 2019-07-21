b9b914e0614247ad10d712d26c452d4217821fd4
Matteo Suppo <matteo.suppo@gmail.com>
2019-07-21 19:36:14 +0200

    hide games that don't follow tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,27 +7,67 @@ class IndexPage extends React.Component {
   constructor(props){
     super(props);
 
-    this.tagsFilter = [];
+    // Populate filters state
+    const tags = {};
+    console.debug(props.data.tags)
+    props.data.tags.group.forEach(tag => {
+      tags[tag.fieldValue] = false; // checked attribute of checkbox
+    });
+
+    this.state = {
+      tags: tags
+    }
+    
+    this.change = this.change.bind(this);
+    this.getClass = this.getClass.bind(this);
+  }
+
+  change(event) {
+    const tags = this.state.tags;
+    tags[event.target.value] = event.target.checked;
+    this.setState({tags: tags});
+  }
+
+  getClass(tags) {
+    const selected = Object.keys(this.state.tags).filter(key => {
+      return this.state.tags[key];
+    })
+    console.debug(selected);
+
+    // If no filters are selected, game is visible
+    if (selected.length === 0){
+      return '';
+    }
+
+    let visible = true;
+    for (const selectedTag of selected) {
+      if (tags.indexOf(selectedTag) === -1) {
+        visible = false;
+      }
+    }
+
+    if (visible) {
+      return '';
+    }
+
+    return 'hidden';
   }
 
   render() {
-
-    console.debug(this.props.data.tags)
-
     return (
     <Layout>
       <SEO title="Home" />
       <h1>Hi people</h1>
 
-      {this.props.data.tags.group.map(node => (
-        <div key={node.fieldValue}>
-          <label>{node.fieldValue}</label>
-          <input type="checkbox" value={node.fieldValue}/>
+      {Object.keys(this.state.tags).map(key => (
+        <div key={key}>
+          <label>{key}</label>
+          <input type="checkbox" value={key} checked={this.state.tags[key]} onChange={ this.change }/>
         </div>
       ))}
 
       {this.props.data.games.edges.map(({ node }) => (
-        <div key={node.id}>
+        <div key={node.id} className={this.getClass(node.frontmatter.tags)}>
           <h3>
             {node.frontmatter.title}{" "}
           </h3>
