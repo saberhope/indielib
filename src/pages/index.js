b5e064f5b244683907b6170763f919ff493fe0cb
Matteo Suppo <matteo.suppo@gmail.com>
2019-07-21 17:31:40 +0200

    convert index in class

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,30 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    {data.allMarkdownRemark.edges.map(({ node }) => (
-      <div key={node.id}>
-        <h3>
-          {node.frontmatter.title}{" "}
-        </h3>
-        {node.html}
-      </div>
-    ))}
-  </Layout>
-)
+class IndexPage extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.tagsFilter = [];
+  }
+
+  render() {
+    return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+
+      {this.props.data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title}{" "}
+          </h3>
+          {node.html}
+        </div>
+      ))}
+    </Layout>
+  )}
+}
 
 export default IndexPage
 
